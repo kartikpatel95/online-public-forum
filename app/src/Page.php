33345c8e1ea393ccef690ddc1419aaf468566c5a
Kartik Patel <kartikpatel@outlook.co.nz>
2019-12-03 15:55:33 +1300

    banner and banner content added to all page types

--- app/src/Page.php
+++ app/src/Page.php
@@ -2,12 +2,41 @@
 
 namespace {
 
+    use SilverStripe\AssetAdmin\Forms\UploadField;
+    use SilverStripe\Forms\HTMLEditor\HtmlEditorField;
+    use SilverStripe\Assets\Image;
     use SilverStripe\CMS\Model\SiteTree;
 
     class Page extends SiteTree
     {
-        private static $db = [];
+        private static $db = [
+          'BannerContent' => 'HTMLText'
+        ];
 
-        private static $has_one = [];
+        private static $has_one = [
+          'Banner' => Image::class
+        ];
+
+        /**
+         * Relationship version ownership
+         * @var array
+         */
+        private static $owns = [
+          'Banner'
+        ];
+
+        /**
+         * CMS Fields
+         * @return FieldList
+         */
+        public function getCMSFields()
+        {
+          $fields = parent::getCMSFields();
+          $fields->addFieldToTab('Root.Banner', HtmlEditorField::create('BannerContent','Banner Content'));
+          $fields->addFieldToTab('Root.Banner', $banner = UploadField::create('Banner','Banner'));
+          $banner->setFolderName('Banners');
+          $banner->setAllowedExtensions(['jpeg', 'jpg', 'png', 'gif']);
+          return $fields;
+        }
     }
 }
