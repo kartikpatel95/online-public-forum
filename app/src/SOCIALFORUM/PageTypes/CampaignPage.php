a5bf80d66d79e759c75225998b945e0e2a1803d2
Kartik Patel <kartikpatel@outlook.co.nz>
2019-12-04 15:08:05 +1300

    teaser and validation added to campaign page

--- app/src/SOCIALFORUM/PageTypes/CampaignPage.php
+++ app/src/SOCIALFORUM/PageTypes/CampaignPage.php
@@ -2,6 +2,15 @@
 
 namespace SOCIALFORUM;
 
+use SilverStripe\Dev\Debug;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\FieldGroup;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\ORM\ValidationResult;
+
 class CampaignPage extends \Page
 {
   /**
@@ -9,4 +18,106 @@ class CampaignPage extends \Page
    * @var string
    */
   private static $table_name = 'CampaignPage';
+
+  /**
+   * Defines whether a page can be in the root of the site tree
+   * @var boolean
+   */
+  private static $can_be_root = true;
+
+  private static $icon_class = "font-icon-menu-campaigns";
+
+  /**
+   * Defines the allowed child page types
+   * @var array
+   */
+  private static $allowed_children = [
+    ForumPage::class
+  ];
+
+  /**
+   * Database fields
+   * @var array
+   */
+  private static $db = [
+    'Active' => 'Boolean',
+    'TileCaption' => 'Varchar'
+  ];
+
+  /**
+   * Add default values to database
+   * @var array
+   */
+  private static $defaults = [
+    'Active' => 0
+  ];
+
+  /**
+   * Has_one relationship
+   * @var array
+   */
+  private static $has_one = [
+    'Teaser' => Image::class
+  ];
+
+  /**
+   * Relationship version ownership
+   * @var array
+   */
+  private static $owns = [
+    'Teaser'
+  ];
+
+  /**
+   * CMS Fields
+   * @return FieldList
+   */
+  public function getCMSFields()
+  {
+    $fields = parent::getCMSFields();
+    $layout = FieldGroup::create([
+      CheckboxField::create('Active', 'Active Campaign')
+    ])->setTitle('Layout');
+    $fields->addFieldToTab('Root.Main', $layout, 'Content');
+
+    $fields->addFieldToTab('Root.Teaser', TextField::create('TileCaption', 'Tile Caption')
+    ->setDescription('Max characters is 30.'));
+    $fields->addFieldToTab('Root.Teaser', $teaser = UploadField::create('Teaser'));
+    $teaser->setFolderName('Teasers');
+    return $fields;
+  }
+
+  public function getCMSValidator()
+  {
+    return RequiredFields::create([
+      'TileCaption', 'Teaser'
+    ]);
+  }
+
+  /**
+  * @return ValidationResult
+  */
+  public function validate()
+  {
+    $result = parent::validate();
+    try {
+      $this->validation($result);
+    }catch (\Exception $exc){
+      $result->addError($exc->getMessage());
+    }
+    return $result;
+  }
+
+  /**
+  * @param ValidationResult $result
+  * @return ValidationResult
+  */
+  public function validation(ValidationResult $result)
+  {
+    $caption = trim($this->TileCaption);
+    if(strlen($caption) > 30){
+      $result->addFieldError('TileCaption', 'Maximum number of characters that are allowed is 30.', 'bad');
+    }
+    return $result;
+  }
 }
